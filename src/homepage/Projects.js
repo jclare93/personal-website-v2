36b99f3e2a075a9903673d7d131a0af3a5ea66c7
jclare93 <85895069+jclare93@users.noreply.github.com>
2022-01-04 15:48:34 -0800

    adding logos to project and images folders
    Adding logos to project component

--- src/homepage/Projects.js
+++ src/homepage/Projects.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import restaurantres from "../images/restaurantres.svg"
+import indexcard from "../images/indexcard.svg"
+import timerround from "../images/timerround.svg"
 
 function Projects(){
     return(
@@ -6,7 +9,7 @@ function Projects(){
         <h3 className ="d-flex justify-content-center py-3">Projects</h3>
         <ul class="list-unstyled">
             <li class="media">
-                <img src="..." class="mr-3" alt="..." />
+                <img src={restaurantres} style= {{height: 64}}className="mr-3" alt="Knife, fork, and plate logo" />
                 <div class="media-body">
                 <h5 class="mt-0 mb-1"><strong>Restaurant Reservation App</strong></h5>
                 <p> This application can be used by business administrators to manage their restaurant's 
@@ -21,9 +24,9 @@ function Projects(){
                 </div>
             </li>
             <li class="media my-4">
-                <img src="..." class="mr-3" alt="..." />
+                <img src={indexcard} style={{height:64}} class="mr-3" alt="file folders" />
                 <div class="media-body">
-                <h5 class="mt-0 mb-1">Flashcards App</h5>
+                <h5 class="mt-0 mb-1"><strong>Flashcards App</strong></h5>
                 <p>This is a web based React application. This is an application developed using mostly 
                 ReactJS and Javascript. This project demonstrates
                 how to build custom hooks and proficiently manage state in react web applications.
@@ -35,10 +38,19 @@ function Projects(){
                 </div>
             </li>
             <li class="media">
-                <img src="..." class="mr-3" alt="..." />
+                <img src={timerround} style = {{height:64}} class="mr-3" alt="round timer logo" />
                 <div class="media-body">
-                <h5 class="mt-0 mb-1">List-based media object</h5>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                <h5 class="mt-0 mb-1"><strong>Pomodoro Timer</strong></h5>
+                <p>This is a React application with an intuitive interface and effective management of state
+                    that allows the app to continue operating seemlessly and continuously.
+                    It is a timer based on the “Pomodoro Method” to help improve time management skills for work or studying.
+                </p>
+                <a href="https://github.com/jclare93/Pomodoro-Timer"><b>GitHub Link</b></a> 
+                ||
+                <a href="https://project-pomodoro-timer-qualified-1-coral-pi.vercel.app/"><b> Live Demo</b></a>
+                <p></p>
+                <p><b>Technology used:</b> JavaScript, HTML, CSS, and React, and deployed on Vercel.</p>
+                
                 </div>
             </li>
         </ul>
